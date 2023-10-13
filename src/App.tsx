468aed062040cb2c8b3d7d7a37deaf195295ce65
Livia1505 <olivia.benoit@epitech.eu>
2023-10-13 16:13:38 +0200

    :sparkles: Add feature to get popular films

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+interface IAPIOptions {
+  method: string;
+  headers: {
+    accept: string;
+    Authorization: string;
+  };
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [films, setFilms] = useState<Object[]>([]);
+
+  const options: IAPIOptions = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODdlYjc4YjUxMWEyOGIwZGRiZTFiYWYzMzFiZDEzNiIsInN1YiI6IjY1MjkzODQ3NTQ0YzQxMGRkNTMwYTJhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xPRGDpWCdj9c2YXX4JXee0Udb6IAxw6zqofS9DmXmII",
+    },
+  };
+
+  async function getFilms() {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1&key=f87eb78b511a28b0ddbe1baf331bd136",
+        options
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setFilms((e) => [...e, result.results]);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  getFilms();
+
+  // console.log(films);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Trouve les films et gagne une joie indescriptible</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
